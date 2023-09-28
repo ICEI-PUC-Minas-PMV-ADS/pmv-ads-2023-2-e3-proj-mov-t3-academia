c1e139e92f161de527fffe6c52fb2cc5ebcba9e5
Ricardo F Pereira <ricardofonseca_1@hotmail.com>
2023-09-28 17:56:03 -0300

    start-fitTraining

---
+++ src/fit-training/App.js
@@ -0,0 +1,15 @@
+import { Text, SafeAreaView, StyleSheet, View } from 'react-native';
+
+import Login from './src/pages/Login';
+
+
+
+export default function App() {
+  return (
+<Login/>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
